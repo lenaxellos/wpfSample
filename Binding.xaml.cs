135e6e92a63d0293b7dc970f0fb38cfa0b9da835
ju chang yeob <lenaxellos@naver.com>
2024-02-24 16:35:35 +0900

    종속Property가 아닌 Property에 대한 Binding

--- Binding.xaml.cs
+++ Binding.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace wpfSample
 {
@@ -19,9 +22,59 @@ namespace wpfSample
     /// </summary>
     public partial class Binding : Window
     {
+        MyFile myFile = new MyFile();
+        string[] files = { };        
+
         public Binding()
         {
             InitializeComponent();
+
+            this.DataContext = myFile;            
+        }
+
+        private void GetfolderFilesName(string folderName)
+        {
+            files = Directory.GetFiles(folderName);
+
+            foreach (string file in files)
+            {
+                myFile.MyFileName = file;
+                cbofiles.Items.Add(file);
+            }
+
+            cbofiles.SelectedIndex = 0;            
+        }
+
+        private void BTNComboSelect_Click(object sender, RoutedEventArgs e)
+        {
+            GetfolderFilesName(@"C:\Users\lenax\Downloads\PC-20240223T133738Z-001\PC");
+        }
+    }
+
+    //알림 통보
+    class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    //파일명 설정
+    class MyFile : Notifier
+    {
+        private string _fileName;
+
+        public string MyFileName
+        {
+            get { return _fileName; }
+            set
+            {
+                _fileName = value;
+                OnPropertyChanged("MyFileName");
+            }
         }
     }
 }
