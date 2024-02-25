65e24a20083162fbe7692a474c1dfa9b9d18073d
ju chang yeob <lenaxellos@naver.com>
2024-02-25 16:35:17 +0900

    ObservableCollection 예제 추가

--- WpfBinding.xaml.cs
+++ WpfBinding.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -23,28 +24,33 @@ namespace wpfSample
     public partial class WpfBinding : Window
     {
         MyFile myFile = new MyFile();
+
+        ObservableCollection<MyFile2> myFile2s = new ObservableCollection<MyFile2>();
+
         string[] files = { };
 
-        public WpfBinding()
+        public WpfBinding()         //Main함수
         {
             InitializeComponent();                       
 
             //2.ElemnetName C# code Binding
             codeBinding();
 
-            //3.Property binding
-            this.DataContext = myFile;            
+            //3.Property binding - INotifyPropertyChanged 사용 통보알림
+            this.DataContext = myFile;
         }
 
-        //2.ElemnetName C# code Binding
+        #region //2.ElemnetName C# code Binding        
         private void codeBinding()
         {
             Binding binding = new Binding();
             binding.Source = txtBox2;                               //원본소스 지정
             binding.Path = new PropertyPath(TextBox.TextProperty);  //원본소스의 Property지정
-            txtBL2.SetBinding(TextBlock.TextProperty, binding);     //타겟에 binding       
+            txtBL2.SetBinding(TextBlock.TextProperty, binding);     //타겟에 binding                                                                   
         }
+        #endregion
 
+        #region//3.Property binding - INotifyPropertyChanged 사용 통보알림
         private void GetfolderFilesName(string folderName)
         {
             files = Directory.GetFiles(folderName);
@@ -55,16 +61,38 @@ namespace wpfSample
                 cbofiles.Items.Add(file);
             }
 
-            cbofiles.SelectedIndex = 0;            
+            cbofiles.SelectedIndex = 0;
         }
 
         private void BTNComboSelect_Click(object sender, RoutedEventArgs e)
         {
             GetfolderFilesName(@"C:\Users\lenax\Downloads\PC-20240223T133738Z-001\PC");
         }
+        #endregion
+
+        #region //4.ObservableCollection - 컬렉션 타입 통보알림    
+        private ObservableCollection<MyFile2> GetFileNames()
+        {
+            myFile2s.Clear();
+
+            files = Directory.GetFiles(@"E:\Study\Book");
+
+            foreach (string file in files)
+            {
+                myFile2s.Add(new MyFile2(file));                
+            }
+
+            return myFile2s;
+        }        
+
+        private void BTNListSelect_Click(object sender, RoutedEventArgs e)
+        {            
+            listBox4.ItemsSource = GetFileNames();
+        }
+        #endregion
     }
 
-    //3.Property binding - 알림 통보
+    #region //3.Property binding - 파일명 설정    
     class Notifier : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
@@ -74,8 +102,7 @@ namespace wpfSample
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-
-    //3.Property binding - 파일명 설정
+    
     class MyFile : Notifier
     {
         private string _fileName;
@@ -90,4 +117,26 @@ namespace wpfSample
             }
         }
     }
+    #endregion
+
+    #region //4.ObservableCollection - 컬렉션 타입 통보알림    
+    class MyFile2
+    {
+        private string _fileName;
+
+        public string MyFileName2
+        {
+            get { return _fileName; }
+            set
+            {
+                _fileName = value;                
+            }
+        }
+        
+        public MyFile2(string fileName)
+        {
+            this.MyFileName2 = fileName;
+        }
+    }
+    #endregion
 }
